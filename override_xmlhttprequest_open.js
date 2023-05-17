3d2062b3b05a9293fdaa69e1726729e21d012156
Michael0500 <jorik-super1@mail.ru>
2023-05-17 14:17:20 +0300

    Update override_xmlhttprequest_open.js

--- override_xmlhttprequest_open.js
+++ override_xmlhttprequest_open.js
@@ -2,9 +2,11 @@
 (function (open) {
     XMLHttpRequest.prototype.open = function () {
         this.addEventListener("readystatechange", function (e) {
-            if (4 === this.readyState && "secret_data" === this.responseText) { // здесь условие когда должны быть переопределены данные ответа
+            if (4 === this.readyState && 'https://mysite.com/custom-url' === this.responseURL && "secret_data" === this.responseText) { // здесь условие когда должны быть переопределены данные ответа
                 Object.defineProperty(this, 'responseText', {writable: true}); // Очень важно сделать свойство responseText изменяемым
+                Object.defineProperty(this, 'respons', {writable: true}); // Очень важно сделать свойство response изменяемым
                 this.responseText = '----NEW_SECRET_DATA----'; // новый ответ который должен быть возвращен
+                this.response = '----NEW_SECRET_DATA----'; // новый ответ который должен быть возвращен
             }
         }, false);
 
