148f22381915ee5301d64e9ad85072879e6635e9
Michael0500 <jorik-super1@mail.ru>
2023-05-17 13:03:36 +0300

    Create override_xmlhttprequest_open.js

---
+++ override_xmlhttprequest_open.js
@@ -0,0 +1,13 @@
+// Метод переопределяет ответ во всех AJAX запросах по заданому условию
+(function (open) {
+    XMLHttpRequest.prototype.open = function () {
+        this.addEventListener("readystatechange", function (e) {
+            if (4 === this.readyState && "secret_data" === this.responseText) { // здесь условие когда должны быть переопределены данные ответа
+                Object.defineProperty(this, 'responseText', {writable: true}); // Очень важно сделать свойство responseText изменяемым
+                this.responseText = '----NEW_SECRET_DATA----'; // новый ответ который должен быть возвращен
+            }
+        }, false);
+
+        return open.apply(this, arguments);
+    };
+})(XMLHttpRequest.prototype.open);
